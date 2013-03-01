89dbe7fbaca157b11938607c4b3e150793a7c588
YuFeng <iscode@qq.com>
2013-03-01 15:39:22 +0800

    add import

---
+++ import.go
@@ -0,0 +1,15 @@
+package gotool
+
+import(
+    _ "github.com/iyf/gotool/config"
+    _ "github.com/iyf/gotool/database/activerecord"
+    _ "github.com/iyf/gotool/i18n"
+    _ "github.com/iyf/gotool/log"
+    _ "github.com/iyf/gotool/middleware"
+    _ "github.com/iyf/gotool/utils"
+    _ "github.com/iyf/gotool/web"
+)
+
+const (
+    VERSION = "0.0.1"
+)
